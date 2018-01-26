9595f221740319a3bca28bcce1d8b8fc3ebbeac1
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-01-26 18:50:05 +0300

    assembly to configs

---
+++ MiniSocialNetwork/MiniSocialNetwork.Shared/AutomapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MiniSocialNetwork.Api.Models;
+using MiniSocialNetwork.Bll.DTO;
+using MiniSocialNetwork.Dal.Entities;
+
+namespace MiniSocialNetwork.Shared
+{
+    public static class AutomapperConfig
+    {
+        public static void InitMapper()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<RegisterViewModel, UserDTO>();
+                cfg.CreateMap<UserDTO, UserProfile>();
+                cfg.CreateMap<UserDTO, ApplicationUser>()
+                    .ForMember();
+            });
+        }
+    }
+}
