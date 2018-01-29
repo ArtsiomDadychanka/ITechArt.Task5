09adf533b9a81ce95f49fe3dc3028d896c10f69c
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-01-29 18:45:51 +0300

    plugged autofac to app

---
+++ MiniSocialNetwork/MiniSocialNetwork.DIConfig/AutofacConfig.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using MiniSocialNetwork.Bll.Interfaces;
+using MiniSocialNetwork.Bll.Services;
+using MiniSocialNetwork.Dal.Interfaces;
+using MiniSocialNetwork.Dal.Repositories;
+using System.Data.SqlClient;
+using System.Data.Entity.Core.EntityClient;
+
+namespace MiniSocialNetwork.DIConfig
+{
+    public static class AutofacConfig
+    {
+        //private static string connectionString = new 
+        //    @"Data Source=.;Initial Catalog=SocialNetwork;Integrated Security=True;providerName=System.Data.SqlClient";
+        public static void Register(ContainerBuilder builder)
+        {
+            builder.RegisterType<UnitOfWork>()
+                .As<IUnitOfWork>()
+                .WithParameter("connectionString", GetConnectionString());
+
+            builder.RegisterType<UserService>()
+                .As<IUserService>();
+        }
+
+        private static string GetConnectionString()
+        {
+            return new SqlConnectionStringBuilder
+            {
+                InitialCatalog = "SocialNetwork",
+                DataSource = ".",
+                IntegratedSecurity = true
+            }.ConnectionString;
+        }
+    }
+}
