6eea6ec4ca7d429233399f023679012ddcb02e8f
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-01-29 18:44:48 +0300

    nothing interesting

---
+++ MiniSocialNetwork/MiniSocialNetwork.Shared/SharedConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSocialNetwork.Shared
+{
+    public static class SharedConfiguration
+    {
+        static SharedConfiguration()
+        {
+            ConnectionString =
+                @"Data Source=(localdb)\v17.0;AttachDbFilename=|DataDirectory|\db.mdf;Integrated Security=True;";
+        }
+
+        public static String ConnectionString { get; } 
+    }
+}
