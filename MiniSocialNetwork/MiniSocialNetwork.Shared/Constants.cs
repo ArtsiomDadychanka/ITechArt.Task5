45b024ce2aa818fc5e063100af9b6729961cd245
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-01-30 17:54:31 +0300

    added constants

---
+++ MiniSocialNetwork/MiniSocialNetwork.Shared/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSocialNetwork.Shared
+{
+    public static class Constants
+    {
+        public static class RolesNames
+        {
+            public static readonly String Admin = "Admin";
+            public static readonly String User = "User";
+        }
+    }
+}
