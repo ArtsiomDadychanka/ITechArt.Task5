8dbb35c7b342ddfcb14837700ec42a875242e187
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-02-01 17:23:59 +0300

    update comment entity

--- MiniSocialNetwork/MiniSocialNetwork.Dal/Entities/Comment.cs
+++ MiniSocialNetwork/MiniSocialNetwork.Dal/Entities/Comment.cs
@@ -17,7 +17,10 @@ namespace MiniSocialNetwork.Dal.Entities
         public DateTime PostedTime { get; set; } = DateTime.Now;
         [ForeignKey("Post")]
         public String PostId { get; set; }
+        [ForeignKey("UserProfile")]
+        public String AuthorId { get; set; }
 
         public virtual Post Post { get; set; }
+        public virtual UserProfile UserProfile { get; set; }
     }
 }
