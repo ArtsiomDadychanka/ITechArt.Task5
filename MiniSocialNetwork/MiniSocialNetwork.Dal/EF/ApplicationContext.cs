397dc3ac189b13a604cb87c3f9da7a321f1c55fe
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-02-01 17:23:40 +0300

    changed context to migrate db

--- MiniSocialNetwork/MiniSocialNetwork.Dal/EF/ApplicationContext.cs
+++ MiniSocialNetwork/MiniSocialNetwork.Dal/EF/ApplicationContext.cs
@@ -9,10 +9,16 @@ namespace MiniSocialNetwork.Dal.EF
     {
         public DbSet<UserProfile> UserProfiles { get; set; }
         public DbSet<Post> UserPosts { get; set; }
+        public DbSet<Comment> Type { get; set; }
 
         static ApplicationContext()
         {
             //System.Data.Entity.Database.SetInitializer(new ContextInitializer());
+        }
+
+        public ApplicationContext() : base("Server=.;initial catalog=SocialNetwork;Integrated Security=true;")
+        {
+            
         }
 
         public ApplicationContext(string connectionString) : base(connectionString)
