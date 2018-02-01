6a893b78e54b89a11c76458ac768cd42875b5733
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-02-01 17:30:40 +0300

    update auth actions in according to server changes

--- MiniSocialNetwork/MiniSocialNetwork.Web/ClientApp/actions/authActions.js
+++ MiniSocialNetwork/MiniSocialNetwork.Web/ClientApp/actions/authActions.js
@@ -1,5 +1,18 @@
-import * as types from '../constants/actionTypes';
+import * as types from './actionTypes';
 import * as global from '../constants/global';
+
+function toServerFormat(user) {
+  const preparedUser = {
+    "grant_type": "password",
+    "username": user.username,
+    "password": user.password,
+  };
+  const dataAsParams = new URLSearchParams();
+  for (const key in preparedUser) {
+    dataAsParams.append(key, preparedUser[key]);
+  }
+  return dataAsParams;
+}
 
 export function signIn(userCredentials) {
   return (dispath) => {
@@ -8,29 +21,34 @@ export function signIn(userCredentials) {
       data: null
     });
 
-    fetch(`${global.SERVER_API_ADDRESS}${global.SERVER_API_URI.SIGNIN}`, {
+    let headers = new Headers({
+      "Accept": "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    });
+    fetch(`${global.SERVER_TOKEN_ADDRESS}`, {
         mode: 'cors',
         method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userCredentials)
+        headers,
+        body: toServerFormat(userCredentials),
       })
       .then(res => {
-        if (res.status === 200) {
-          dispath({
-            type: types.SIGN_IN_SUCCESS,
-            data: userCredentials,
-            redirectUrl: `${global.SERVER_ADDRESS}${global.PAGES_URL.USER_PROFILE}`
-          });
-        } else {
+        if (res.status !== 200) {
           dispath({
             type: types.SIGN_IN_REJECT,
             data: null,
             error: res.statusText,
           });
         }
+        return res.json();
+      })
+      .then(json => {
+        const token = json[global.ACCESS_TOKEN_KEY];
+        sessionStorage.setItem(global.TOKEN_KEY, token);
+        dispath({
+          type: types.SIGN_IN_SUCCESS,
+          data: json, // TODO: must be only username!
+          redirectUrl: `${global.SERVER_ADDRESS}${global.PAGES_URL.USER_PROFILE}`
+        });
       })
       .catch(err => console.log(err));
   };
