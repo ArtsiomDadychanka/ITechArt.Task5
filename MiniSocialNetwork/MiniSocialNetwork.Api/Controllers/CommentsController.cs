c14e9795b1b3ee0fa33b7cc186daf2bb438d9a89
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-02-02 17:00:33 +0300

    implemented comment logic

--- MiniSocialNetwork/MiniSocialNetwork.Api/Controllers/CommentsController.cs
+++ MiniSocialNetwork/MiniSocialNetwork.Api/Controllers/CommentsController.cs
@@ -3,37 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using AutoMapper;
+using MiniSocialNetwork.Api.Models;
+using MiniSocialNetwork.Bll.DTO;
+using MiniSocialNetwork.Bll.Interfaces;
 
 namespace MiniSocialNetwork.Api.Controllers
 {
+    [RoutePrefix("api/comments")]
     public class CommentsController : ApiController
     {
-        // GET: api/Comment
-        public IEnumerable<string> Get()
+        private ICommentService commentService;
+
+        public CommentsController(ICommentService commentService)
         {
-            return new string[] { "value1", "value2" };
+            this.commentService = commentService;
         }
 
-        // GET: api/Comment/5
-        public string Get(int id)
+        [Route("{postId:Guid}")]
+        public async Task<IHttpActionResult> Get(Guid postId)
         {
-            return "value";
+            if (postId.Equals(Guid.Empty))
+            {
+                return BadRequest("Invalid post id");
+            }
+
+            var comments = await commentService.GetPostCommentsAsync(postId.ToString());
+            IEnumerable<DisplayedCommentViewModel> displayedComments =
+                Mapper.Map<IEnumerable<CommentDTO>, IEnumerable<DisplayedCommentViewModel>>(comments);
+
+            return Ok(displayedComments);
         }
 
-        // POST: api/Comment
-        public void Post([FromBody]string value)
+        [Route("")]
+        public async Task<IHttpActionResult> Post([FromBody]CreatedCommentViewModel commentViewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await commentService.CreateCommentAsync(Mapper.Map<CreatedCommentViewModel, CommentDTO>(commentViewModel));
+
+            if (!result.Succedeed)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Created("", commentViewModel);
         }
 
-        // PUT: api/Comment/5
-        public void Put(int id, [FromBody]string value)
+        [Route("{id:Guid}")]
+        public async Task<IHttpActionResult> Delete(Guid id)
         {
-        }
+            if (id.Equals(Guid.Empty))
+            {
+                return BadRequest("Invalid comment id");
+            }
 
-        // DELETE: api/Comment/5
-        public void Delete(int id)
-        {
+            var result = await commentService.RemoveCommentAsync(id.ToString());
+
+            if (!result.Succedeed)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok(result.Message);
         }
     }
 }
