beeac32f5ea88b9d69240ddcd6fd3c44f8b71b1f
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-02-05 17:48:51 +0300

    plugged jquery for materialize.css

--- MiniSocialNetwork/MiniSocialNetwork.Web/App_Start/BundleConfig.cs
+++ MiniSocialNetwork/MiniSocialNetwork.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,8 @@ namespace MiniSocialNetwork.Web
             bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
                 "~/node_modules/materialize-css/dist/js/materialize.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/node_modules/jquery/dist/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/main").Include(
                 "~/public/bundle.js"));
