4fb4f88f03fdf8520a28850c1f49e7ce485a2e5b
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2018-02-08 16:57:34 +0300

    added entities to map

--- MiniSocialNetwork/MiniSocialNetwork.Api/Configs/AutomapperConfig.cs
+++ MiniSocialNetwork/MiniSocialNetwork.Api/Configs/AutomapperConfig.cs
@@ -21,11 +21,11 @@ namespace MiniSocialNetwork.Api.Configs
             cfg.CreateMap<LoginViewModel, UserDTO>();
             cfg.CreateMap<RegisterViewModel, UserDTO>();
             cfg.CreateMap<UserDTO, DisplayedUserViewModel>()
-                .ForMember("Username", opt => opt.MapFrom(f => $"{f.Firstname} {f.Lastname}"));
+                .ForMember(src => src.Username, opt => opt.MapFrom(f => $"{f.Firstname} {f.Lastname}"));
 
             cfg.CreateMap<CreatedPostViewModel, PostDTO>();
             cfg.CreateMap<PostDTO, DisplayedPostViewModel>()
-                .ForMember("LikeCounts", opt => opt.MapFrom(f => f.Likes));
+                .ForMember(src => src.LikeCounts, opt => opt.MapFrom(f => f.Likes));
 
             cfg.CreateMap<CreatedCommentViewModel, CommentDTO>();
             cfg.CreateMap<CommentDTO, DisplayedCommentViewModel>();
@@ -36,20 +36,26 @@ namespace MiniSocialNetwork.Api.Configs
         {
             cfg.CreateMap<PostDTO, Post>();
             cfg.CreateMap<Post, PostDTO>()
-                .ForMember("AuthorName",
+                .ForMember(src => src.AuthorName,
                     opt => opt.MapFrom(f => $"{f.UserProfile.Firstname} {f.UserProfile.Lastname}"))
-                .ForMember("AuthorId", opt => opt.MapFrom(f => f.UserProfile.ApplicationUser.Id));
+                .ForMember(src => src.AuthorId, opt => opt.MapFrom(f => f.UserProfile.ApplicationUser.Id));
 
             cfg.CreateMap<CommentDTO, Comment>();
             cfg.CreateMap<Comment, CommentDTO>()
-                .ForMember("AuthorName",
+                .ForMember(src => src.AuthorName,
                     opt => opt.MapFrom(f => $"{f.UserProfile.Firstname} {f.UserProfile.Lastname}"))
-                .ForMember("AuthorId", opt => opt.MapFrom(f => f.UserProfile.ApplicationUser.Id));
+                .ForMember(src => src.AuthorId, opt => opt.MapFrom(f => f.UserProfile.ApplicationUser.Id));
 
             cfg.CreateMap<UserDTO, UserProfile>();
             cfg.CreateMap<UserProfile, UserDTO>();
             cfg.CreateMap<UserDTO, ApplicationUser>()
-                .ForMember("UserName", opt => opt.MapFrom(f => f.Email));
+                .ForMember(src => src.UserName, opt => opt.MapFrom(f => f.Email));
+
+            cfg.CreateMap<Dialog, DialogDTO>();
+            cfg.CreateMap<DialogDTO, Dialog>();
+
+            cfg.CreateMap<Message, MessageDTO>();
+            cfg.CreateMap<MessageDTO, Message>();
         }
     }
 }
